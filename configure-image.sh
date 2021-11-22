c55c709f707788c00030b9b3b36076fcded16aa5
Lars Kellogg-Stedman <lars@oddbit.com>
2021-11-22 15:22:39 -0500

    Ensure required ports are open in firewalld configuration

--- configure-image.sh
+++ configure-image.sh
@@ -14,3 +14,17 @@ yum -y install @workstation
 yum -y --enablerepo=epel install xrdp
 systemctl enable xrdp
 systemctl set-default graphical.target
+
+cat > /etc/firewalld/zones/public.xml <<'EOF'
+<?xml version="1.0" encoding="utf-8"?>
+<zone>
+  <short>Public</short>
+  <description>For use in public areas. You do not trust the other computers on networks to not harm your computer. Only selected incoming connections are accepted.</description>
+  <service name="ssh"/>
+  <service name="dhcpv6-client"/>
+  <service name="cockpit"/>
+  <service name="rdp"/>
+  <service name="http"/>
+  <service name="https"/>
+</zone>
+EOF
