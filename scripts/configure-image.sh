6c7a99595910b5b37771880093dcb9ec76ab814d
Lars Kellogg-Stedman <lars@oddbit.com>
2022-07-07 11:34:38 -0400

    Update for summer 2022/fedora 36
    Part of cci-moc/ops-issues#620

--- scripts/configure-image.sh
+++ scripts/configure-image.sh
@@ -4,22 +4,37 @@
 # desktop environment by default. You can apply the script to an image
 # using the virt-customize script:
 #
-#     virt-customize -a CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2 \
+#     virt-customize -a image.qcow2 \
 #       --run configure-image.sh --selinux-relabel
 
+set -e
+
+exec > /root/config.log 2>&1
+
+if [ -b /dev/sda ]; then
+	DEV=/dev/sda
+elif [ -b /dev/vda ]; then
+	DEV=/dev/vda
+else
+	exit 1
+fi
+
+growpart $DEV 5
+btrfs filesystem resize max /
+
 yum -y upgrade
-yum -y install epel-release
-yum config-manager --set-disabled epel --set-disabled epel-modular
-yum -y install @workstation
-yum -y --enablerepo=epel install xrdp
+yum -y install @workstation-product-environment
+yum -y install xrdp
 systemctl enable xrdp
-systemctl set-default graphical.target
+
+grub2-mkconfig -o /boot/grub2/grub.cfg
 
 cat > /etc/firewalld/zones/public.xml <<'EOF'
 <?xml version="1.0" encoding="utf-8"?>
 <zone>
   <short>Public</short>
-  <description>For use in public areas. You do not trust the other computers on networks to not harm your computer. Only selected incoming connections are accepted.</description>
+  <description>For use in public areas. You do not trust the other computers on networks to not harm you
+ computer. Only selected incoming connections are accepted.</description>
   <service name="ssh"/>
   <service name="dhcpv6-client"/>
   <service name="cockpit"/>
